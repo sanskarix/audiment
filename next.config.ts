21a4ef5f2559d5048a9040412ea15db9db3b8346
Sanskar Yadav <sanskar@Sanskars-MacBook-Air.local>
2026-04-08 10:29:48 +0530

    build error3

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-import createMDX from '@next/mdx';
-
-const withMDX = createMDX({});
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       {
@@ -23,8 +20,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     optimizePackageImports: ['lucide-react', 'framer-motion', '@radix-ui/react-icons', 'recharts'],
-    mdxRs: true,
   },
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
